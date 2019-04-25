988d022a634a971c3732253e635a9680f8f1e425
prielango <codeacademy2019@gmail.com>
2019-04-25 16:14:23 +0300

    bla

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php
 
 $arr = [
-	[ 'first_name' => 'Petras', 'last_name' => 'Pizdzius', 'thing' => 'telefonas', 'status' => 'prapistas' ],
-	[ 'first_name' => 'Tomas', 'last_name' => 'Ablomas', 'thing' => 'buljonas', 'status' => 'naudojamas' ],
-	[ 'first_name' => 'Ana', 'last_name' => 'Shitkova', 'thing' => 'rulonas', 'status' => 'pasibaiges' ]
+	[ 
+		'first_name' => 'Petras',
+		'last_name' => 'Pizdzius',
+		'thing' => 'telefonas',
+		'status' => 'prapistas'
+	 ],
+	[
+		'first_name' => 'Tomas',
+		'last_name' => 'Ablomas',
+		'thing' => 'buljonas',
+		'status' => 'naudojamas'
+	],
+	[ 
+		'first_name' => 'Ana',
+		'last_name' => 'Shitkova',
+		'thing' => 'rulonas',
+		'status' => 'pasibaiges'
+	]
 ];
 
 var_dump($arr);
