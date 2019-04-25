f0b615083e497b007ada5f6220b2068c926fb6e6
prielango <codeacademy2019@gmail.com>
2019-04-25 15:26:42 +0300

    nu

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 $arr = [
-	'kiausiniai' => [ 'amount' => 2, 'size' => 'didelis' ],
-	'baklazanai' => [ 'amount' => 1 'size' => 'mazas' ],
-	'grietine' => [ 'amount' => 1 'size' => 'mazas' ]
+	'kiausiniai' => [ 'amount' => 2, 'size' => 'didelis', 'name' => 'kausinis' ],
+	'baklazanai' => [ 'amount' => 1, 'size' => 'mazas', 'name' => 'baklazanas' ],
+	'grietine' => [ 'amount' => 1, 'size' => 'mazas', 'name' => 'grietine' ]
 ];
 
 var_dump($arr);
